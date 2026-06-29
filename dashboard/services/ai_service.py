22fcee132258589f75410d4497b165b2d1f70bd8
Sony-co <siddx450@gmail.com>
2026-06-29 20:21:00 +0530

    xa

--- dashboard/services/ai_service.py
+++ dashboard/services/ai_service.py
@@ -10,43 +10,13 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 #Full Generic Ai Function that returns the output of all the agents
 
 def get_ai_review(prompt):
-
     # 1. Understanding Agent
-    print("understanding agent")
-    understanding = client.chat.completions.create(
-        model="openai/gpt-oss-120b",
-        messages=[{"role": "user", "content": prompt}]
-    )
-
-
-    # 2. Evaluation Agent
-    print("evaluating agent")
-    prompt2 = evaluate_prompt(understanding)
-    evaluation = client.chat.completions.create(
-        model="openai/gpt-oss-120b",
-        messages=[{"role": "user", "content": prompt2}]
-    )
-
-
-    # 3. Roadmap Agent
-    print ("roadmap")
-    prompt3 = roadmap_prompt(understanding, evaluation)
-    roadmap = client.chat.completions.create(
-        model="openai/gpt-oss-120b",
-        messages=[{"role": "user", "content": prompt3}]
-    )
-
 
     # 4. Founder Report Agent
     print ("founder")
-    prompt4 = founder_report_prompt(
-        understanding,
-        evaluation,roadmap
-    )
-
     founder = client.chat.completions.create(
         model="openai/gpt-oss-120b",
-        messages=[{"role": "user", "content": prompt4}]
+        messages=[{"role": "user", "content": prompt}]
     )
 
     content = founder.choices[0].message.content
